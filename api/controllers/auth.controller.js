385a36e6d5a82ed0a24cefb03a2a7e806bd94d3c
MarkoMilenovic <markomilenovic7@gmail.com>
2024-10-06 15:46:02 +0200

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from '../models/user.model.js'
 import { errorHandler } from '../utils/error.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+dotenv.config();
 
 
 export const signup = async (req, res, next) => {
@@ -16,4 +19,34 @@ export const signup = async (req, res, next) => {
     } catch (error) {
       next(error);
     }
-  };
+};
+
+
+export const signin = async (req, res, next) => {  // Add 'next' here
+    const { email, password } = req.body;
+
+    try {
+        // Check if user exists
+        const validUser = await User.findOne({ email : email });
+        if (!validUser) return next(errorHandler(404, "User not found!"));
+        console.log("User:", validUser);
+        console.log("Password from DB:", validUser.password);
+        console.log("Entered Password:", password);
+
+        // Validate password
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+
+        // Generate JWT token
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const {password : pass, ...rest} = validUser._doc;
+        // Set token as httpOnly cookie
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        // Pass the error to the next middleware
+        next(error);
+    }
+};
